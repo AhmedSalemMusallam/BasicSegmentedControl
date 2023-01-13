280f66d36eec2aa06a2f02099b2b32e8dae08746
Ahmed Salem Musallam Ali <117483832+AhmedSalemMusallam@users.noreply.github.com>
2023-01-13 03:21:52 +0200

    Basic Segmented Control Projection Creation - Commit

---
+++ BasicSegmentedControl/BasicSegmentedControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicSegmentedControl
+//
+//  Created by Ahmed Salem on 13/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
